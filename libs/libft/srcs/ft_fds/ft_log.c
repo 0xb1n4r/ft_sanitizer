f84cb5e8fd68ba1622919b28bb1ef153f6859384
hypn0x <leetpwnsroot@gmail.com>
2022-02-20 15:57:21 +0100

    fm_printf in logs

--- libs/libft/srcs/ft_fds/ft_log.c
+++ libs/libft/srcs/ft_fds/ft_log.c
@@ -14,12 +14,12 @@
 void *ft_malloc_log(int len, int line, const char *filename)
 {
 	void *ptr = malloc(len);
-	ft_printf(1, "M %d %p %s %d\n",len, ptr,  filename, line);
+	fm_printf(1, "M %d %p %s %d\n",len, ptr,  filename, line);
 	return ptr;
 }
 
 void ft_free_log(void *p, const char *filename, int line)
 {
-	ft_printf(1, "F %p %s %d\n", p, filename, line);
+	fm_printf(1, "F %p %s %d\n", p, filename, line);
 	free(p);
 }
